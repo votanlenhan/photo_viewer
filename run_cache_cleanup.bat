d4753fd4de5847bdfc16e2bb20b942fdcd42cfed
Lucas Vo <votanlenhan@gmail.com>
2025-04-22 16:06:10 +0700

    Fix clear cache 1

--- run_cache_cleanup.bat
+++ run_cache_cleanup.bat
@@ -5,11 +5,11 @@ cd /d "%~dp0"
 echo Current Directory After CD: %CD% >> logs\cron-debug.log
 
 echo === Running Cache Cleanup: %date% %time% === >> logs\cron-output.log
-php.exe cron_cache_manager.php >> logs\cron-output.log 2>&1 
+"C:\xampp\php\php.exe" cron_cache_manager.php >> logs\cron-output.log 2>&1 
 echo Cache Cleanup Exit Code: %errorlevel% >> logs\cron-debug.log
 
 echo === Running Log Cleanup: %date% %time% === >> logs\cron-output.log
-php.exe cron_log_cleaner.php >> logs\cron-output.log 2>&1
+"C:\xampp\php\php.exe" cron_log_cleaner.php >> logs\cron-output.log 2>&1
 echo Log Cleanup Exit Code: %errorlevel% >> logs\cron-debug.log
 
 echo === Finished Maintenance Tasks: %date% %time% === >> logs\cron-output.log 
